221c14dcdb91dba239f3c2632c57f1caa9466215
Fabrice Bellingard <bellingard@gmail.com>
2026-07-20 16:45:25 +0200

    Improve nav

--- src/lib/nav.ts
+++ src/lib/nav.ts
@@ -1,9 +1,9 @@
 export const navItems = [
   { label: "About", href: "/" },
   { label: "Experience", href: "/experience" },
+  { label: "Skills", href: "/skills" },
   { label: "Impact", href: "/impact" },
   { label: "Open source", href: "/open-source" },
-  { label: "Skills", href: "/skills" },
   { label: "Publications", href: "/publications" },
   { label: "Social", href: "/social" },
 ] as const;
